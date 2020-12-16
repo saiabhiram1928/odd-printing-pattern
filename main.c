8f2e92abc83bdac97ddaeb54956b763e58b7b482
saiabhiram1928 <75559376+saiabhiram1928@users.noreply.github.com>
2020-12-16 23:32:38 +0530

    Add files via upload

---
+++ main.c
@@ -0,0 +1,34 @@
+
+#include <stdio.h>
+
+
+int main()
+{ int j,i;
+for(i=1;i<6;i++){
+    if(i==1){
+        printf("*");
+    }else{
+        for(j=1;j<=i;j++){
+            if(j==1){
+                printf("*");
+            }else{
+                printf("**");
+            }
+        }
+    }printf("\n");
+    }
+
+    return 0;
+}
+
+
+
+          
+        
+
+
+
+   
+
+
+    
